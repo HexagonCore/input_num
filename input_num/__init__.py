b79c5b92a2a3425c4f21adec91308c3476d88fc6
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2021-07-06 12:45:11 +0200

    start

--- input_num/__init__.py
+++ input_num/__init__.py
@@ -17,9 +17,22 @@ import time
 
 lvers = "0.0.1"
 
+class CallableModule():
+
+    def __init__(self, wrapped):
+        self._wrapped = wrapped
+
+    def __call__(self, *args, **kwargs):
+        return self._wrapped.main(*args, **kwargs)
+
+    def __getattr__(self, attr):
+        return object.__getattribute__(self._wrapped, attr)
+
+sys.modules[__name__] = CallableModule(sys.modules[__name__])
+
 
 def checkver():
-    packagenm = 'nput_num'
+    packagenm = 'input_num'
     responseinfl = requests.get(f'https://pypi.org/pypi/{packagenm}/json')
     latest_version = responseinfl.json()['info']['version']
     if latest_version != lvers:
@@ -29,9 +42,18 @@ def checkver():
 checkver()
 
 
+def main(val, option = "false"):
+    global output
+    try:
+        output = input(val)
+        nothing = float(int(output))
+    except:
+        main(val, option)
+    finally:
+        return output
+
 
 #!------------------------------!
 #!       End                    !
 #!                              !
 #!------------------------------!
-
