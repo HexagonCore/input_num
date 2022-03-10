850af28bbf72ed768bd2005a021779a5053fe604
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-03-10 14:37:50 +0100

    .

--- input_num/__init__.py
+++ input_num/__init__.py
@@ -33,8 +33,6 @@ sys.modules[__name__] = CallableModule(sys.modules[__name__])
 
 def online():
     try:
-        # connect to the host -- tells us if the host is actually
-        # reachable
         sock = socket.create_connection(("www.google.com", 80))
         if sock is not None:
             sock.close
